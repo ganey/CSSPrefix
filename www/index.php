05bf254d5500623cc114dbcf51081a0e01bc9b70
Germain Metti <germain@italic.fr>
2016-03-04 11:17:28 +0100

    fix design

--- www/index.php
+++ www/index.php
@@ -16,14 +16,14 @@ if ($prefix && $css) {
 
 		$comments = explode("*/", $part);
 		foreach ($comments as &$comment) {
-			
+
 			$subParts = explode(',', $comment);
 			foreach ($subParts as &$subPart) {
 				// vérifier que c'est une classe ou un id
 				//echo $subPart.' => '.substr(trim($subPart), 0, 1).'<br>';
 				if (substr(trim($subPart), 0, 1) == '.') {
 					$subPart = $prefix . $spacer . ($subPart);
-				} 
+				}
 
 				elseif (substr(trim($subPart), 0, 1) == '#') {
 					//echo $subPart.' => '.substr(trim($subPart), 0, 1).'<br>';
@@ -74,7 +74,7 @@ if ($prefix && $css) {
 </head>
 
 <body>
-	<a href="https://github.com/italic"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+	<a href="https://github.com/italic"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 9999;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 	<div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container">
 			<div class="navbar-header">
@@ -132,7 +132,6 @@ if ($prefix && $css) {
 		<br>
 		<!-- GitHub Watch -->
 		<a class="github-button" href="https://github.com/italic/CSSPrefix" data-icon="octicon-eye" data-style="mega" aria-label="Watch italic/CSSPrefix on GitHub">Watch</a>
-		<small><a href="http://www.italic.fr" target="_blank">This is yet another <em>italic</em> production.</a></small>
 		<!-- GitHub Star -->
 		<a class="github-button" href="https://github.com/italic/CSSPrefix" data-icon="octicon-star" data-style="mega" aria-label="Star italic/CSSPrefix on GitHub">Star</a>
 		<!-- GitHub Fork -->
@@ -141,7 +140,9 @@ if ($prefix && $css) {
 		<a class="github-button" href="https://github.com/italic/CSSPrefix/issues" data-icon="octicon-issue-opened" data-style="mega" aria-label="Issue italic/CSSPrefix on GitHub">Issue</a>
 		<br>
 		<br>
-
+		<small><a href="http://www.italic.fr" target="_blank">This is yet another <em>italic</em> production.</a></small>
+		<br>
+		<br>
 	</div><!-- /.container -->
 
 	<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
